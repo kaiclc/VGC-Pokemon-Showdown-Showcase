ba8d6138009debe8896ad4a8c37c578b944ea6c7
ruirui42 <rui.jiang2007@gmail.com>
2025-04-22 17:42:05 -0400

    Update PokemonFenwickTree.cpp

--- PokemonFenwickTree.cpp
+++ PokemonFenwickTree.cpp
@@ -1,13 +1,13 @@
 #include "PokemonFenwickTree.hpp"
 
-PokemonFenwickTree::PokemonFenwickTree(const vector<int>& battles, const unordered_map<string, vector<int>>& realCounts) //constructor
+PokemonFenwickTree::PokemonFenwickTree(const vector<int>& battles, const unordered_map<string_view, vector<int>>& realCounts) //constructor
 : totalBattlesTree(battles) {
 	for (const auto& group : realCounts) {
 		pokemonTrees.emplace(group.first, FenwickTree(group.second));
 	}
 }
 
-tuple<int, int> PokemonFenwickTree::getUsage(const string& pokemonName, int startIndex, int endIndex) { //gets pokemon usage
+tuple<int, int> PokemonFenwickTree::getUsage(const string_view& pokemonName, int startIndex, int endIndex) { //gets pokemon usage
 	auto it = pokemonTrees.find(pokemonName);
 	if (it == pokemonTrees.end()) {
 		return { 0, 0 };
