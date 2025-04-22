e4de5d43fbfee9a8a9aa0cf3142dc30ff8118e1c
ruirui42 <rui.jiang2007@gmail.com>
2025-04-22 17:41:46 -0400

    Update PokemonFenwickTree.hpp

--- PokemonFenwickTree.hpp
+++ PokemonFenwickTree.hpp
@@ -1,13 +1,17 @@
 #pragma once
 #include "FenwickTree.hpp"
+#include "simdjson.h"
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <string>
+using namespace std;
+using namespace simdjson;
 
 class PokemonFenwickTree {
 	FenwickTree totalBattlesTree;
-	unordered_map<string, FenwickTree> pokemonTrees;
+	unordered_map<string_view, FenwickTree> pokemonTrees;
 public:
-	PokemonFenwickTree(const vector<int>& battles, const unordered_map<string, vector<int>>& realCounts);
-	tuple<int, int> getUsage(const string& pokemonName, int startIndex, int endIndex);
+	PokemonFenwickTree(const vector<int>& battles, const unordered_map<string_view, vector<int>>& realCounts);
+	tuple<int, int> getUsage(const string_view& pokemonName, int startIndex, int endIndex);
 };
